61fb0f0e67e5b2857ed142da3b5df107527ba0c5
Mike <mike.pham@autogeneral.com.au>
2025-05-09 10:03:55 +1000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+function caesarEncrypt(plainText: string, shift: number): string {
+    return '';
+}
+
+function caesarDecript(cipherText: string, shift: number): string {
+    return '';
+}
+
+// Extension
+function caesarDecryptGuess(cipherText: string): string {
+    return '';
+}
