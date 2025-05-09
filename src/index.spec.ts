150a947213ff56e6d232b1a468b0653b1c83fb8f
Mike <mike.pham@autogeneral.com.au>
2025-05-09 13:34:15 +1000

    Reorder tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -11,19 +11,19 @@ function assertEqual(actual: string, expected: string, message: string) {
 
 // Basic shift test
 assertEqual(caesarEncrypt("abc", 1), "bcd", "Shift lowercase by +1");
-assertEqual(caesarEncrypt("ABC", 1), "BCD", "Shift uppercase by +1");
-
-// Wrap-around test
 assertEqual(caesarEncrypt("xyz", 3), "abc", "Wrap around end of lowercase");
-assertEqual(caesarEncrypt("XYZ", 3), "ABC", "Wrap around end of uppercase");
 
-// Decryption reverses encryption
-const msg = "The quick brown fox jumps over the lazy dog!";
+const msg = "the quick brown fox jumps over the lazy dog!";
 const shift = 5;
 const encrypted = caesarEncrypt(msg, shift);
 const decrypted = caesarDecrypt(encrypted, shift);
 assertEqual(decrypted, msg, "Decrypt reverses encrypt");
 
+// Wrap-around test
+assertEqual(caesarEncrypt("ABC", 1), "BCD", "Shift uppercase by +1");
+assertEqual(caesarEncrypt("XYZ", 3), "ABC", "Wrap around end of uppercase");
+
+// Decryption reverses encryption
 // Negative shift
 assertEqual(caesarEncrypt("bcd", -1), "abc", "Negative shift works");
 
