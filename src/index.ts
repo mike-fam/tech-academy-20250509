4061b576aaa4bc23a47d332a7244811717764a6a
Mike <mike.pham@autogeneral.com.au>
2025-05-09 14:01:24 +1000

    solution without heuristic

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,50 @@
-function caesarEncrypt(plainText: string, shift: number): string {
-    return '';
+const prompt = require('prompt-sync')({ sigint: true });
+
+
+const ALPHA = "abcdefghijklmnopqrstuvwxyz";
+const CHARACTER_FREQUENCY = ['e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z']
+
+function encryptChar(char: string, shift: number) {
+    const plainIndex = ALPHA.indexOf(char);
+    if (plainIndex === -1) {
+        return char;
+    }
+    const cipherIndex = (((plainIndex + shift) % ALPHA.length) + ALPHA.length) % ALPHA.length;
+    return ALPHA[cipherIndex];
 }
 
-function caesarDecript(cipherText: string, shift: number): string {
-    return '';
+export function caesarEncrypt(plainText: string, shift: number): string {
+    let result = "";
+    for (const char of plainText) {
+        let encryptedChar = encryptChar(char.toLowerCase(), shift);
+        if (char === char.toUpperCase()) {
+            encryptedChar = encryptedChar.toUpperCase();
+        }
+        result += encryptedChar;
+    }
+    return result;
+}
+
+export function caesarDecrypt(cipherText: string, shift: number): string {
+    return caesarEncrypt(cipherText, ALPHA.length - shift);
 }
 
 // Extension
+function getSortedCharsByFrequency(input: string): string[] {
+    const freqMap: Record<string, number> = {};
+
+    for (const char of input) {
+      const lowerChar = char.toLowerCase(); 
+      if (ALPHA.includes(char)) {
+          freqMap[lowerChar] = (freqMap[lowerChar] || 0) + 1;
+      }
+    }
+
+    return Object.keys(freqMap).sort((a, b) => freqMap[b] - freqMap[a]);
+}
+
 function caesarDecryptGuess(cipherText: string): string {
+    const sortedCharByFrequency = getSortedCharsByFrequency(cipherText);
     return '';
 }
+
